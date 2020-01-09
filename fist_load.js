e55dc4810a1db95a662080e36480f5a4d37682bc
ddz <ddzyan@163.com>
2020-01-09 15:41:05 +0800

    删除 apollo.js 配置文件，修改为直接读取config.[环境变量].js , 环境变量由启动时传入 , 默认为 local

--- fist_load.js
+++ fist_load.js
@@ -2,18 +2,23 @@
 
 const fs = require('fs');
 const path = require('path');
+const assert = require('assert');
+const apollo = require('egg-apollo-ddz');
 
-const apollo = require('./lib/apollo');
+const env = process.env.NODE_ENV || 'local';
 
-const apolloConfigFilePath = path.join(__dirname, '/config/apollo.json');
+const confilgFileName = `config.${env}.js`;
+
+const apolloConfigFilePath = path.join(__dirname, '/config/', confilgFileName);
 if (fs.existsSync(apolloConfigFilePath)) {
   const configFilePath = path.join(__dirname, '/config/apollo-config.json');
   if (fs.existsSync(configFilePath)) {
     fs.unlinkSync(configFilePath);
   }
 
-  const apolloConfig = require(apolloConfigFilePath);
-  apollo.remoteConfigServiceSikpCache(apolloConfig).then(config => {
+  const apolloConfig = require(apolloConfigFilePath)();
+  assert(apolloConfig.apolloDdz, 'apolloDdz 配置 不存在');
+  apollo.remoteConfigServiceSikpCache(apolloConfig.apolloDdz).then(config => {
     const configStr = JSON.stringify(config);
     fs.appendFileSync(configFilePath, configStr);
     // fs.unlinkSync(configFilePath);
